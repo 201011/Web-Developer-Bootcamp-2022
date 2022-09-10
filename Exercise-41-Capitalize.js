d59a017fc3215488709a23cff15aedad8cf6d8f4
SWARAJ RAJENDRA MUTALIK <71939130+swarajmutalik@users.noreply.github.com>
2022-09-10 12:28:22 +0530

    Create Exercise-41-Capitalize.js

---
+++ Exercise-41-Capitalize.js
@@ -0,0 +1,8 @@
+function capitalize(text){
+    let text1 = text[0].toUpperCase() + text.slice(1,text.length);
+    return text1;
+}
+
+capitalize('eggplant') 
+capitalize('pamplemousse') 
+capitalize('squid') 
